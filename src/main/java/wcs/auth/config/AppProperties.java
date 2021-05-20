36072cf5a832663569cb8f2cc0bffbaa29cc2ded
yonmoyonmo <yonmosound@gmail.com>
2021-05-21 05:16:26 +0900

    되는데 왜 되는지 모르겠음

--- src/main/java/wcs/auth/config/AppProperties.java
+++ src/main/java/wcs/auth/config/AppProperties.java
@@ -7,24 +7,43 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.ArrayList;
 import java.util.List;
 
-@ConfigurationProperties(prefix="app")
+@ConfigurationProperties(prefix = "app")
 public class AppProperties {
-
     private final Auth auth = new Auth();
     private final OAuth2 oauth2 = new OAuth2();
 
-    @Getter
-    @Setter
-    public static final class Auth {
+    public static class Auth {
         private String tokenSecret;
         private long tokenExpirationMsec;
+
+        public String getTokenSecret() {
+            return tokenSecret;
+        }
+
+        public void setTokenSecret(String tokenSecret) {
+            this.tokenSecret = tokenSecret;
+        }
+
+        public long getTokenExpirationMsec() {
+            return tokenExpirationMsec;
+        }
+
+        public void setTokenExpirationMsec(long tokenExpirationMsec) {
+            this.tokenExpirationMsec = tokenExpirationMsec;
+        }
     }
 
-    @Getter
-    @Setter
     public static final class OAuth2 {
         private List<String> authorizedRedirectUris = new ArrayList<>();
 
+        public List<String> getAuthorizedRedirectUris() {
+            return authorizedRedirectUris;
+        }
+
+        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
+            this.authorizedRedirectUris = authorizedRedirectUris;
+            return this;
+        }
     }
 
     public Auth getAuth() {
